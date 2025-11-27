d10e74aa1a94a24b92ecab96752a0af23afab315
Rubem Alcantara <rubemmatheus2012@gmail.com>
2025-11-27 10:29:51 -0300

    Implementando categorias e relacionamento entre tabelas

--- gestao/admin.py
+++ gestao/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from .models import Produto
+from .models import Produto, Categoria
+
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ('nome',)  # Colunas da tabela.
+    search_fields = ('nome',)  # Barra de pesquisa.
 
 @admin.register(Produto)
 class ProdutoAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'preco', 'quantidade', 'validade') # Colunas da tabela
-    search_fields = ('nome',) # Barra de pesquisa
-    list_filter = ('validade',) # Filtro lateral por data
+    list_display = ('nome', 'preco', 'quantidade', 'validade') # Colunas da tabela.
+    search_fields = ('nome',) # Barra de pesquisa.
+    list_filter = ('validade', 'categoria') # Filtro lateral por data e categoria.
+    
--- gestao/models.py
+++ gestao/models.py
@@ -1,16 +1,20 @@
 from django.db import models
 
+class Categoria(models.Model):
+    nome = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.nome
+
 class Produto(models.Model):
 
     nome = models.CharField(max_length=100)
-
     descricao = models.TextField()
-
     preco = models.DecimalField(max_digits=10, decimal_places=2)
-
     validade = models.DateField()
-
     quantidade = models.IntegerField(default=0)
+
+    categoria = models.ForeignKey(Categoria, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return self.nome
