a316ea6b91df67dfa5a2bb0eca9cf8699ee758e7
Rubem Alcantara <rubemmatheus2012@gmail.com>
2025-11-29 23:53:32 -0300

    Implementação do Dashboard Administrativo (KPIs e Alertas) com restrição de acesso para Staff

--- gestao/urls.py
+++ gestao/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('editar/<int:produto_id>/', views.produto_edit, name='produto_edit'),
     path('excluir/<int:produto_id>/', views.produto_delete, name='produto_delete'),
     path('accounts/register/', views.register, name='register'),
+    path('dashboard/', views.dashboard, name='dashboard'),
 ]
--- gestao/views.py
+++ gestao/views.py
@@ -1,7 +1,13 @@
+from django.db.models import Sum
+from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import Group
 from .forms import ProdutoForm, UsuarioForm
-from .models import Produto
+from .models import Produto, Categoria
+
+def checar_admin(user):
+    return user.is_staff
 
 def produto_list(request):
     
@@ -51,7 +57,12 @@ def register(request):
     if request.method == 'POST':
         form = UsuarioForm(request.POST)
         if form.is_valid():
-            form.save()
+            usuario = form.save(commit=False)
+            usuario.save()
+
+            grupo_vendedores = Group.objects.get(name='Vendedores')
+            usuario.groups.add(grupo_vendedores)
+            
             return redirect('login')
     
     else:
@@ -59,6 +70,28 @@ def register(request):
 
     return render(request, 'registration/register.html', {'form': form})
 
+def dashboard(request):
+    total_produtos = Produto.objects.count()
+    total_categorias = Categoria.objects.count()
+    
+    #Filtrando produtos com menos de 5 unidades em estoque
+    produtos_baixo_estoque_queryset = Produto.objects.filter(quantidade__lt=5)
+    
+    #Contagem para o carg estoque
+    baixo_estoque_count = produtos_baixo_estoque_queryset.count()
+
+    #Somando o valor total do estoque
+    valor_estoque = Produto.objects.aggregate(Sum('preco'))['preco__sum'] or 0
+
+    contexto = {
+        'total_produtos': total_produtos,
+        'total_categorias': total_categorias,
+        'baixo_estoque': baixo_estoque_count,
+        'lista_alerta': produtos_baixo_estoque_queryset,
+        'valor_estoque': valor_estoque,
+        }
+
+    return render(request, 'gestao/dashboard.html', contexto)
 
 
 
